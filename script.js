09d4c3a5f6853148083a0ffeac92fcdf54291b77
AJ5154 <ashishjadhav597@gmail.com>
2023-12-19 13:13:22 +0530

    complete assignment

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const generateButton = document.querySelector(".btn-primary");
+  
+    generateButton.addEventListener("click", function () {
+      generateBoxes();
+    });
+  
+    function generateBoxes() {
+      const numBlocks = parseInt(document.querySelector(".form-control").value);
+  
+      const existingBoxes = document.querySelectorAll(".box");
+      existingBoxes.forEach((box) => box.remove());
+  
+      const boxSize = Math.min(window.innerWidth, window.innerHeight) / numBlocks;
+  
+      for (let i = 0; i < numBlocks; i++) {
+        const box = document.createElement("div");
+        box.className = "box";
+        document.body.appendChild(box);
+  
+        const xPos = i * boxSize;
+        const yPos = i * boxSize;
+  
+        box.style.width = `${boxSize*2.215}px`;
+        box.style.height = `${boxSize*0.83}px`;
+        box.style.left = `${xPos*2.212}px`;
+        box.style.bottom = `${yPos*0.83}px`;
+      }
+    }
+  });
+  
